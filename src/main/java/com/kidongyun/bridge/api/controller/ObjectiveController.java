88a96f1c04ab6c6f8c7789dd13e92eb0001d5569
Kidong Yun <oriondigestive@gmail.com>
2021-01-01 12:21:48 +0900

    JACKSON PROBLEM IS SOLVED

--- src/main/java/com/kidongyun/bridge/api/controller/ObjectiveController.java
+++ src/main/java/com/kidongyun/bridge/api/controller/ObjectiveController.java
@@ -1,5 +1,7 @@
 package com.kidongyun.bridge.api.controller;
 
+import com.kidongyun.bridge.api.entity.Cell;
+import com.kidongyun.bridge.api.entity.Objective;
 import com.kidongyun.bridge.api.repository.objective.ObjectiveRepository;
 import com.kidongyun.bridge.api.service.ObjectiveService;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Slf4j
 @RestController
 @RequestMapping("api/v1/objective")
@@ -21,6 +26,13 @@ public class ObjectiveController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getObjective(@PathVariable("id") Long id) {
+        Optional<Objective> objOpt = objectiveRepository.findById(1L);
+
+        try {
+            log.info("YKD : " + objOpt.orElseThrow(() -> new Exception("Obj is not found")).toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return ResponseEntity.status(HttpStatus.OK).body(HttpStatus.OK.getReasonPhrase());
     }
 }
--- src/test/java/com/kidongyun/bridge/api/controller/ObjectiveControllerTest.java
+++ src/test/java/com/kidongyun/bridge/api/controller/ObjectiveControllerTest.java
@@ -1,13 +1,21 @@
 package com.kidongyun.bridge.api.controller;
 
+import com.kidongyun.bridge.api.config.QuerydslConfig;
+import com.kidongyun.bridge.api.entity.Cell;
+import com.kidongyun.bridge.api.entity.Objective;
+import com.kidongyun.bridge.api.repository.objective.ObjectiveRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.LocalDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -15,7 +23,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Import(QuerydslConfig.class)
 public class ObjectiveControllerTest {
+    @Autowired
+    ObjectiveRepository objectiveRepository;
+
     @Autowired
     ObjectiveController objectiveController;
 
@@ -28,8 +40,21 @@ public class ObjectiveControllerTest {
 
     @Test
     public void getObjective_normal() throws Exception {
+        /* Arrange */
+        objectiveRepository.save(Objective.builder().id(1L).type(Cell.Type.Objective).startDateTime(LocalDateTime.now()).endDateTime(LocalDateTime.now())
+                .status("completed").title("title1").description("desc1").build());
+
+        objectiveRepository.save(Objective.builder().id(2L).type(Cell.Type.Objective).startDateTime(LocalDateTime.now()).endDateTime(LocalDateTime.now())
+                .status("prepared").title("title2").description("desc2").parent(Objective.builder().id(1L).build()).build());
+
+        objectiveRepository.save(Objective.builder().id(3L).type(Cell.Type.Objective).startDateTime(LocalDateTime.now()).endDateTime(LocalDateTime.now())
+                .status("prepared").title("title3").description("desc3").parent(Objective.builder().id(1L).build()).build());
+
+        objectiveRepository.save(Objective.builder().id(4L).type(Cell.Type.Objective).startDateTime(LocalDateTime.now()).endDateTime(LocalDateTime.now())
+                .status("prepared").title("title4").description("desc4").parent(Objective.builder().id(2L).build()).build());
+
         mockMvc.perform(get("/api/v1/objective/1")
-                .param("", ""))
+                .param("id", "1"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
